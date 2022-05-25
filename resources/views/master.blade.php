c2d84f58c8b0272701e096f3d4fed014565f2697
tomtomturbo <thomasreist@gmail.com>
2022-05-25 13:47:09 +0200

    first commit

---
+++ resources/views/master.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <!-- hier wird der Wert von der section "title" eines blade templates ausgefüllt,
+   welches dieses layout "extended" -->
+   <title>@yield('title')</title>
+</head>
+<body>
+   <!-- hier wird auch der Wert von der section "title" eines blade templates ausgefüllt,
+   welches dieses layout "extended" -->
+   <h1><a href="/messages">@yield('title')</a></h1>
+   <!-- hier wird der Wert von der section "content" eines blade templates ausgefüllt,
+   welches dieses layout "extended" -->
+   @yield('content')
+   <!-- hier wird die php Funktion date() aufgerufen mit dem Format-Pattern 'd.m.Y'
+    und im html ausgegeben-->
+   <div><b>Dummy Footer: {{date('d.m.Y')}}</b></div>   
+</body>
+</html>
---
+++ resources/views/messages.blade.php
@@ -0,0 +1,14 @@
+<!--extend layout master.blade.php -->
+ 
+@extends('master')
+ 
+<!--sets value for section title to "Mini Twitter" (section title is used in messages.blade.php) -->
+@section('title', 'Mini Twitter')
+ 
+<!--starts section content, defines some html for section content and end section content
+ts value for section title to "Mini Twitter" (section content is used in messages.blade.php) -->
+@section('content')
+ 
+<h2>Recent messages:</h2>
+ 
+@endsection
